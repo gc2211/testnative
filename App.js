1c117aa5413f7945a911348a16e75415b5a84534
gauthier-chatel_student2021@wilder.school <88785506+gc2211@users.noreply.github.com>
2022-02-08 13:34:25 +0100

    First reactnative . Needs improvement

--- App.js
+++ App.js
@@ -1,11 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TextInput, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 446534,
+      text: "Buy cheese"
+    },
+    {
+      id: 456454,
+      text: "Buy wine"
+    },
+    {
+      id: 131234,
+      text: "Buy apple"
+    },
+    {
+      id: 498753,
+      text: "Buy crackers"
+    },
+    {
+      id: 324684,
+      text: "Buy nuts"
+    }
+  ])
+
+  const deleteHandler = (id) => {
+    setTodos(prevValue => prevValue.filter(item => item.id !== id))
+  };
+
+  const addNewTodoHandler = () => {
+    setTodos(prevValue => [...prevValue, {id: Math.random(), text: newTodo}]);
+    setNewTodo("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text style={styles.text}>{count}</Text>
+        <Button
+          title="increment"
+          onPress={() => setCount(prevValue => prevValue + 1)}
+        />
+        <Button
+          title="decrement"
+          onPress={() => setCount(prevValue => prevValue - 1)}
+        />
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.containerBig}>
+
+        <TextInput
+          placeholder="enter a new todo"
+          value={newTodo}
+          onChangeText={setNewTodo}
+        />
+        <Button
+          title="add"
+          onPress={addNewTodoHandler}
+        />
+        
+        <FlatList
+          data={todos}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.listItem}>
+                <Text style={styles.list}>{item.text}</Text>
+                <Button
+                  title="❌"
+                  onPress={() => deleteHandler(item.id)}
+                />
+              </View>
+            )
+          }}
+          keyExtractor={item => item.id}
+        />
+
+      </View>
     </View>
   );
 }
@@ -17,4 +92,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerBig: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50,
+  },
+  list: {
+    fontSize: 20
+  },
+  listItem: {
+    flexDirection: 'row'
+  }
 });
